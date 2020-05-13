16851452122ea34875e9fc86c715d8f97cd23595
Eric Pohlers <e.pohlers@web.de>
2020-05-13 12:24:21 +0200

    add debounce function for event delay

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
 const input = document.querySelector('input');
 
-let timeoutId;
-const onInput = (event) => {
-  const searchTerm = event.target.value;
-  if (timeoutId) {
-    clearTimeout(timeoutId);
-  }
-  if (searchTerm) {
-    timeoutId = setTimeout(() => {
-      fetchData(searchTerm);
-    }, 500);
-  }
-};
-input.addEventListener('input', onInput);
-
 const fetchData = async (searchTerm) => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
@@ -24,3 +10,21 @@ const fetchData = async (searchTerm) => {
 
   console.log(response.data);
 };
+
+const debounce = (func, delay = 1000) => {
+  let timeoutId;
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func.apply(null, args);
+    }, delay);
+  };
+};
+
+const onInput = (event) => {
+  fetchData(event.target.value);
+};
+
+input.addEventListener('input', debounce(onInput, 500));
