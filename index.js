585d1c97166c60d59ee72e8fc898bf26996b89ee
Eric Pohlers <e.pohlers@web.de>
2020-05-13 12:10:20 +0200

    add searchfield; set timer for axios-fetch on event input

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-const fetchData = async () => {
+const input = document.querySelector('input');
+
+let timeoutId;
+const onInput = (event) => {
+  const searchTerm = event.target.value;
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+  if (searchTerm) {
+    timeoutId = setTimeout(() => {
+      fetchData(searchTerm);
+    }, 500);
+  }
+};
+input.addEventListener('input', onInput);
+
+const fetchData = async (searchTerm) => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: API_KEY,
-      s: 'avengers',
+      s: searchTerm,
     },
   });
 
   console.log(response.data);
 };
-
-fetchData();
